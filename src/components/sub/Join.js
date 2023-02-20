48ddf6c21580483becd5c4d105465d5eb861eb4b
jjune96 <hj09060906@gmail.com>
2023-02-20 15:01:27 +0900

    join 기본 틀

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -3,7 +3,79 @@ import Layout from '../common/Layout';
 function Join() {
 	return (
 		<Layout name={'Join / 회원가입'}>
-			<p>Join</p>
+			<form>
+				<fieldset>
+					<legend className='h'>회원가입 폼 양식</legend>
+					<table>
+						<tbody>
+							{/* user id */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='useid'>User Id</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										name='useid'
+										id='useid'
+										placeholder='id를 입력하세요'
+									/>
+								</td>
+							</tr>
+							{/* password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd1'>Password</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd1'
+										placeholder='비밀번호를 입력하세요'
+									/>
+								</td>
+							</tr>
+
+							{/* re password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd2'>Re-Password</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd2'
+										placeholder='비밀번호를 재입력하세요'
+									/>
+								</td>
+							</tr>
+
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>E-mail</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										name='email'
+										id='email'
+										placeholder='email주소를 입력해주세요'
+									/>
+								</td>
+							</tr>
+
+							{/* btnSet */}
+							<tr>
+								<th colspan='2'>
+									<input type='reset' value='cancel' />
+									<input type='submit' value='send' />
+								</th>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -4,7 +4,6 @@ import { useRef, useEffect } from 'react';
 function Location() {
 	const container = useRef(null);
 
-	//윈도우 객체에서 kakao 객체를 비구조화할당으로 바로 할당
 	const { kakao } = window;
 
 	const option = {
@@ -12,53 +11,22 @@ function Location() {
 		level: 3,
 	};
 
-	//const map = new kakao.maps.Map(container.current, option);
-
-	// // const hideTraffic = map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-
-	// const [Show, setShow] = map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-
-	// const showTraffic = new kakao.maps.Map(container.current, option).addOverlayMapTypeId(
-	// 	kakao.maps.MapTypeId.TRAFFIC
-	// );
-
-	// const closeTraffic = new kakao.maps.Map(
-	// 	container.current,
-	// 	option
-	// ).removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-
-	//마커 위치 인스턴스 생성
 	const markerPosition = option.center;
 
-	//실제 마커 생성
 	const marker = new kakao.maps.Marker({
 		position: markerPosition,
 	});
 
 	useEffect(() => {
 		const mapInstance = new kakao.maps.Map(container.current, option);
-		// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
 		const mapTypeControl = new kakao.maps.MapTypeControl();
 
-		// 지도 타입 컨트롤을 지도에 표시합니다
 		mapInstance.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-		//마커 생성 호출구문
 		marker.setMap(mapInstance);
 
-		// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
 		const zoomControl = new kakao.maps.ZoomControl();
 		mapInstance.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-		// mapInstance.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-
-		// function showTraffic() {
-		// 	mapInstance.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-		// }
-
-		// function closeTraffic() {
-		// 	mapInstance.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-		// }
 
 		function getInfo() {
 			// 지도의 현재 중심좌표를 얻어옵니다
@@ -95,6 +63,21 @@ function Location() {
 			message +=
 				'북동쪽 좌표는 ' + neLatLng.getLat() + ', ' + neLatLng.getLng() + ' 입니다';
 		}
+		//지도 가운데 이동 함수
+		const setCenter = () => {
+			let moveLatLon = new kakao.maps.LatLng(35.1631139, 129.1635509);
+
+			mapInstance.setCenter(moveLatLon);
+		};
+		mapInstance.setZoomable(false);
+
+		//브라우저 리사이즈 될떄마다 마커 가운데 위치시키는 함수 호출
+		window.addEventListener('resize', setCenter);
+
+		//해당 컴포넌트 unmount시 setCenter함수 이벤트 제거
+		return () => {
+			window.removeEventListener('resize', setCenter);
+		};
 	}, []);
 
 	function showTraffic() {
