5c87929d315ea56552817f523fc737ddd3427d02
jjune96 <hj09060906@gmail.com>
2023-03-02 16:15:57 +0900

    swiper 버그 수정 (8버전으로 변경)

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,23 +1,61 @@
 import { memo } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 function Vids() {
 	const Vids = useSelector((store) => store.youtube.data);
 
 	return (
 		<section id='vids' className='myScroll'>
-			<h1>Youtube</h1>
-			{Vids.map((vid, idx) => {
-				if (idx >= 3) return null;
+			<Swiper
+				navigation={true}
+				grabCursor={true}
+				modules={[Autoplay, Navigation, Pagination]}
+				//slidesPerView={3}
+				spaceBetween={50}
+				loop={true}
+				centeredSlides={true}
+				pagination={{ clickable: true }}
+				autoplay={{
+					delay: 2000,
+					disableOnInteraction: true,
+				}}
+				observer={true}
+				observeParents={true}
+			>
+				{Vids.map((vid, idx) => {
+					if (idx >= 6) return null;
 
-				return (
-					<article key={idx}>
-						<div className='pic'>
-							<img src={vid.snippet.thumbnails.medium.url} alt={vid.snippet.title} />
-						</div>
-					</article>
-				);
-			})}
+					return (
+						<SwiperSlide key={idx}>
+							<div className='inner'>
+								<div className='pic'>
+									<img src={vid.snippet.thumbnails.maxres.url} alt={vid.snippet.title} />
+								</div>
+								<h2>
+									{vid.snippet.title.length >= 30
+										? vid.snippet.title.substr(0, 40) + '...'
+										: vid.snippet.title}
+								</h2>
+								<p>
+									{vid.snippet.description.length >= 150
+										? vid.snippet.description.substr(0, 150) + '...'
+										: vid.snippet.description}
+								</p>
+								<li>
+									<NavLink to='/youtube'>VIEW OUR YOUTUBE</NavLink>
+								</li>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
 		</section>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-dom';
+import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import youtubeReducer from './redux/youtubeSlice';
 
@@ -11,6 +11,7 @@ const store = configureStore({
 		youtube: youtubeReducer,
 	},
 });
+
 ReactDOM.render(
 	<React.StrictMode>
 		<HashRouter>
@@ -22,19 +23,14 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-//npm i redux react-redux
-
 /*
-redux-saga vs redux-thunk
-
-redux-saga :
--비동기 데이터의 호출 및 리듀서 상태변화 관련 함수를 saga에서 generator함수로 동기화 처리
--컴포넌트 외부에서 독립적으로 비동기 데이터를 전역관리하기 위한 라이브러리
-
-redux-thunk :
--action 객체 안쪽에 비동기 데이터 호출 함수 및 리듀서 상태변화 관리 함수를 같이 등록
-
-redux-toolkit :
--redux-thunk 기반의 통합 라이브러리
--기존의 action개채 생성을 slice라는 파일 형태로 컴포넌트 외부에서 간단하게 설정 가능
+	redux-saga vs redux-thunk
+	redux-saga : 
+	- 비동기 데이터의 호출 및 리듀서 상태변화 관련 함수를 saga에서 generator함수로 동기화 처리
+	- 컴포넌트 외부에서 독립적으로 비동기 데이터를 전역관리하기 위한 라이브러리
+	redux-thunk : 
+	- action객체 안쪽에 비동기 데이터 호출 함수 및 리듀서 상태변화 관리 함수를 같이 등록
+	redux-toolkit :
+	- redux-thunk기반의 통합 라이브러리
+	- 기존의 action개체 생성을 slice라는 파일 형태로 컴포넌트 외부에서 간단하게 설정가능
 */
